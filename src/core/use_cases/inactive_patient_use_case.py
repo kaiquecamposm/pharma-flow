6540f1f32feec24cf7e946f86de8ce1ce4954a39
kaiquecamposdev <kaiquedecampos2@gmail.com>
2025-09-10 14:01:33 -0300

    refactor: remove unused patient-related classes and use cases

--- src/core/use_cases/inactive_patient_use_case.py
+++
@@ -1,9 +0,0 @@
-class DeactivatePatientUseCase:
-    def __init__(self, patient_repository):
-        self.patient_repository = patient_repository
-
-    def execute(self, patient_id: str) -> bool:
-        """
-        Deactivate a patient without physical deletion.
-        """
-        return self.patient_repository.inactivate(patient_id)
