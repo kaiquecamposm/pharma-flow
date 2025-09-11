4250cf470b10fcb021c3a715232cc8dd64f364d5
kaiquecamposdev <kaiquedecampos2@gmail.com>
2025-09-11 18:56:09 -0300

    refactor: remove unused Role and UserRole entity classes

--- src/core/entities/role.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-from core.entities.base import BaseEntity
-
-
-@dataclass
-class Role(BaseEntity):
-    role_name: str = ""
--- src/core/entities/user_role.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class UserRole:
-    professional_id: str
-    role_id: str
